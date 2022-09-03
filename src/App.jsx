539a109deda54909814eb3093d38dcf1907cf9f0
john-glay <johnglayc.bunao@gmail.com>
2022-09-03 18:42:40 +0800

    products & product page

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import NavigationBar from "./components/NavigationBar";
+import Products from "./components/pages/Products";
+import Product from "./components/pages/Product";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/product/:id" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import Hero from "../Hero";
+import Collection from "../Collection";
+import Specials from "../Specials";
+import Banner from "../Banner";
+import Blogs from "../Blogs";
+import Footer from "../Footer";
 
 export default function Home() {
-  return (<h1>Hello World!</h1>);
+  return (
+    <>
+      <Hero />
+      <Collection />
+      <Specials />
+      <Banner />
+      <Blogs />
+      <Footer />
+    </>
+  );
 }
